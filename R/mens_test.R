6d71d620ae8ccd62b0d8ebc187735a63bd54b050
volleydork <chad@volleyvision.com>
2023-03-09 10:12:42 -0800

    updated a couple logos

--- R/mens_test.R
+++ R/mens_test.R
@@ -1,25 +1,25 @@
-attempts <- aggregate(count ~ player_name*team, subset(epv_data, skill == "Attack"), sum)
+attempts <- aggregate(count ~ player_name*team, subset(mens_test, skill == "Attack"), sum)
 colnames(attempts) <- c("player_name", "team", "attempts")
 
-kills <- aggregate(count ~ player_name*team, subset(epv_data, skq == "Attack #"), sum)
+kills <- aggregate(count ~ player_name*team, subset(mens_test, skq == "Attack #"), sum)
 colnames(kills) <- c("player_name", "team", "kills")
 
-insys <- aggregate(poss_eff ~ player_name*team, subset(epv_data, skill == "Attack" & substr(attack_code, 1, 1) %in% c("C", "P", "X")), mean)
+insys <- aggregate(poss_eff ~ player_name*team, subset(mens_test, skill == "Attack" & substr(attack_code, 1, 1) %in% c("C", "P", "X")), mean)
 colnames(insys) <- c("player_name", "team", "insys")
 
-oos <- aggregate(poss_eff ~ player_name*team, subset(epv_data, skill == "Attack" & substr(attack_code, 1, 1) == "V"), mean)
+oos <- aggregate(poss_eff ~ player_name*team, subset(mens_test, skill == "Attack" & substr(attack_code, 1, 1) == "V"), mean)
 colnames(oos) <- c("player_name", "team", "oos")
 
-oos_count <- aggregate(count ~ player_name*team, subset(epv_data, skill == "Attack" & substr(attack_code, 1, 1) == "V"), sum)
+oos_count <- aggregate(count ~ player_name*team, subset(mens_test, skill == "Attack" & substr(attack_code, 1, 1) == "V"), sum)
 colnames(oos_count) <- c("player_name", "team", "oos_count")
 
-epv_added_total <- aggregate(epv_added ~ player_name*team, subset(epv_data, skill == "Attack"), sum)
+epv_added_total <- aggregate(epv_added ~ player_name*team, subset(mens_test, skill == "Attack"), sum)
 colnames(epv_added_total) <- c("player_name", "team", "epv_added_total")
 
-epv_added_avg <- aggregate(epv_added ~ player_name*team, subset(epv_data, skill == "Attack"), mean)
+epv_added_avg <- aggregate(epv_added ~ player_name*team, subset(mens_test, skill == "Attack"), mean)
 colnames(epv_added_avg) <- c("player_name", "team", "epv_added_avg")
 
-epv_ratio <- aggregate(epv_ratio ~ player_name*team, subset(epv_data, skill == "Attack"), mean)
+epv_ratio <- aggregate(epv_ratio ~ player_name*team, subset(mens_test, skill == "Attack"), mean)
 colnames(epv_ratio) <- c("player_name", "team", "epv_ratio")
 
 df <- merge(attempts, kills)
@@ -34,36 +34,66 @@ df$oos <- round(df$oos, 3)
 df$oos <- ifelse((df$oos_percent < 15) | (is.na(df$oos_percent)), " - ", df$oos)
 df$epv_added_per10 <- 10*df$epv_added_avg
 
-schools <- data.frame(c("University of California, Irvine (Men's)",
-                       "California State University, Northridge (Men's)",
-                       "University of California, San Diego (Men's)",
-                       "University of California, Santa Barbara (Men's)",
-                       "University of Hawaii (Men's)",
-                       "California State University, Long Beach (Men's)",
-                       "Pepperdine University (Men's)",
-                       "Stanford University (Men's)",
-                       "Concordia University Irvine (Men's)",
-                       "Grand Canyon University (Men's)",
-                       "University of California, Los Angeles (Men's)",
-                       "Brigham Young University (Men's)",
-                       "University of Southern California (Men's)"),
-                     c("https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/uci.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/csun.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ucsd.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ucsb.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/hawaii.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/lbsu.png",
+schools <- data.frame(c("Lewis University (Men's)",
+                        "Ball State University (Men's)",
+                        "Pepperdine University (Men's)",
+                        "Stanford University (Men's)",
+                        "Concordia University Irvine (Men's)",
+                        "Loyola University Chicago (Men's)",
+                        "University of California, Irvine (Men's)",
+                        "Grand Canyon University (Men's)",
+                        "Purdue University Fort Wayne (Men's)",
+                        "California State University, Northridge (Men's)",
+                        "Ohio State University (Men's)",
+                        "University of California, San Diego (Men's)",
+                        "University of California, Santa Barbara (Men's)",
+                        "University of California, Los Angeles (Men's)",
+                        "Brigham Young University (Men's)",
+                        "University of Southern California (Men's)",
+                        "Lindenwood University (Men's)",
+                        "McKendree University (Men's)",
+                        "Pennsylvania State University (Men's)",
+                        "Quincy University (Men's)",
+                        "Princeton University (Men's)",
+                        "George Mason University (Men's)",
+                        "New Jersey Institute of Technology (Men's)",
+                        "Harvard University (Men's)",
+                        "University of Charleston (WV) (Men's)",
+                        "University of Hawaii (Men's)",
+                        "California State University, Long Beach (Men's)",
+                        "Saint Francis University (Men's)"),
+                     c("https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/lewis.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ballstate.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/pepperdine.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/stanford.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/cui.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/loyola.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/uci.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/gcu.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/pfw.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/csun.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ohiostate.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ucsd.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ucsb.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/ucla.png",
                        "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/byu.png",
-                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/usc.png"))
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/usc.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/lindenwood.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/mckendree.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/pennstate.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/quincy.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/princeton.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/georgemason.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/njit.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/harvard.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/charleston.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/hawaii.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/lbsu.png",
+                       "https://raw.githubusercontent.com/volleydork/volleyR/main/ncaa_logos/sfu.png"))
 colnames(schools) <- c("team", "wordmark")
 
 chart <- merge(df, schools, all.x = TRUE)
-chart <- subset(chart, attempts > 50)
+chart <- subset(chart, attempts > 100)
 chart$mean <- mean(chart$epv_added_avg)
 chart$sd <- sd(chart$epv_added_avg)
 chart$z_score <- (chart$epv_added_avg - chart$mean) / chart$sd
@@ -102,6 +132,6 @@ chart %>%
   #gtExtras::gt_hulk_col_numeric(z_score) %>%
   gt_color_rows(z_score, palette = "ggsci::blue_material") %>%
   gtExtras::gt_img_rows(wordmark) %>%
-  gt::tab_header(title = "Best Attackers in the MPSF/Big West - 2023")
+  gt::tab_header(title = "Best Attackers in Men's Volleyball - 2023")
 w#gtsave(save_me, "mpsf-bigwest-2023-v1.png")
 
