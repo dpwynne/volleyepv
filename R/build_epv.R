e1e03ef29e504fd4c844180191eb1dae064968f8
volleydork <chad@volleyvision.com>
2023-03-12 21:40:32 -0700

    opponent conference

--- R/build_epv.R
+++ R/build_epv.R
@@ -551,8 +551,9 @@ build_epv_average <- function(my.files) {
   epv_data <- merge(epv_data, positions, all.x = TRUE)
 
   epv_data <- merge(epv_data, team_conference_logos, all.x = TRUE)
+  epv_data <- merge(epv_data, opp_conference_logos, all.x = TRUE)
 
-  epv_data <- epv_data %>% relocate(player_name, skq, input_type, output_type, epv_in, epv_out, epv_added, match_date, team, position, conference, opponent, skill, two_touch_ago, one_touch_ago, one_touch_future, two_touch_future, input_kcode, output_kcode, blockers)
+  epv_data <- epv_data %>% relocate(player_name, skq, input_type, output_type, epv_in, epv_out, epv_added, position, match_date, team, conference, opponent, opp_conference, skill, two_touch_ago, one_touch_ago, one_touch_future, two_touch_future, input_kcode, output_kcode, blockers)
   epv_data <- epv_data[order(epv_data$id_touch),]
   print("Boom! Done.")
   View(epv_data)
