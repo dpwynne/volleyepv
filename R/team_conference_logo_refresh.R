64b45de3756fdcbae0878113f407de9c37084fcb
volleydork <chad@volleyvision.com>
2023-03-29 15:42:53 -0700

    remove some scripts

--- R/team_conference_logo_refresh.R
+++
@@ -1,8 +0,0 @@
-library(readxl)
-
-team_conference_logos <- read_excel("teams.xlsx")
-opp_conference_logos <- team_conference_logos
-colnames(opp_conference_logos) <- c("opponent", "opponent_short", "opp_conference", "opp_conference_short", "opp_link", "opp_conference_link")
-
-usethis::use_data(team_conference_logos, overwrite = TRUE)
-usethis::use_data(opp_conference_logos, overwrite = TRUE)
